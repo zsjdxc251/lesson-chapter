1fe7cb24ffd265b1062147a7a6e9dbd9484fd5e6
郑石军 <zsj@vibeac.com>
2018-07-27 18:33:23 +0800

    zk

---
+++ lesson-distributed/chapter-zookeeper/src/main/java/com/chapter/distributed/zookeeper/ConnectionSample.java
@@ -0,0 +1,31 @@
+package com.chapter.distributed.zookeeper;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author zhengshijun
+ * @version created on 2018/7/27.
+ */
+public class ConnectionSample {
+
+    private static final Logger log = LoggerFactory.getLogger(ConnectionSample.class);
+    public static void main(String[] args){
+
+        String url = "";
+        try {
+            ZooKeeper zooKeeper = new ZooKeeper(url,4000,null);
+            
+
+
+
+        } catch (IOException e) {
+            log.error(StringUtils.EMPTY,e);
+        }
+
+    }
+}
