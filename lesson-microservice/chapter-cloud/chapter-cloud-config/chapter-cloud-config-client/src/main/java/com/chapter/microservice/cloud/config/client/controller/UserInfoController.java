805cb732eda224d6f5421f1c4a7914628fc362ec
zhengshijun <zsjdxc251@live.com>
2018-08-02 19:55:19 +0800

    lookupPath

--- lesson-microservice/chapter-cloud/chapter-cloud-config/chapter-cloud-config-client/src/main/java/com/chapter/microservice/cloud/config/client/controller/UserInfoController.java
+++ lesson-microservice/chapter-cloud/chapter-cloud-config/chapter-cloud-config-client/src/main/java/com/chapter/microservice/cloud/config/client/controller/UserInfoController.java
@@ -1,11 +1,16 @@
 package com.chapter.microservice.cloud.config.client.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author zhengshijun
@@ -16,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class UserInfoController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserInfoController.class);
+
     @Value("${login.username}")
     private String loginUsername;
 
     @GetMapping("/loginName")
-    public ResponseEntity<String> loginName(){
+    public ResponseEntity<String> loginName(HttpServletRequest request){
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+
+        String lookupPath = urlPathHelper.getLookupPathForRequest(request);
+        log.info("lookupPath:{}",lookupPath);
         return ResponseEntity.ok(loginUsername);
     }
 }
