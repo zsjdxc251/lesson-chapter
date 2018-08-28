6320ad6df43b064543f8e6fc5306716f4b0242d1
zhengshijun <zsjdxc251@live.com>
2018-08-28 21:36:10 +0800

    @NoRepositoryBean

--- lesson-microservice/chapter-boot/chapter-boot-jpa/src/main/java/com/lesson/microservice/boot/jpa/repository/UserInfoRepository.java
+++ lesson-microservice/chapter-boot/chapter-boot-jpa/src/main/java/com/lesson/microservice/boot/jpa/repository/UserInfoRepository.java
@@ -1,8 +1,9 @@
 package com.lesson.microservice.boot.jpa.repository;
 
 import com.lesson.microservice.boot.jpa.entity.UserInfo;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
+//@NoRepositoryBean
 public interface UserInfoRepository extends PagingAndSortingRepository<UserInfo,Long> {
 }
