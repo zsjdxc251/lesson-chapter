3af9c3d435e962275b450d73278a2f93b13daf85
zhengshijun <zsjdxc251@sina.com>
2018-09-28 00:07:07 +0800

    redis client

---
+++ lesson-distributed/chapter-redis/chapter-redis-sample/src/main/java/com/lesson/distributed/redis/sample/sentinel/SentinelSample.java
@@ -0,0 +1,19 @@
+package com.lesson.distributed.redis.sample.sentinel;
+
+/**
+ * @author zhengshijun
+ * @version created on 2018/9/27.
+ */
+public class SentinelSample {
+
+
+    public static void main(String[] args){
+
+
+//        HostAndPort hostAndPort = new HostAndPort();
+//
+//        // 哨兵集群地址
+//        JedisSentinelPool jedisSentinelPool = new JedisSentinelPool();
+
+    }
+}
