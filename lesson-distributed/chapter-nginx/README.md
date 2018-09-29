8da49411dd7436cb6a7cf69d6842a6ef43e7f802
zhengshijun <zsjdxc251@sina.com>
2018-09-29 23:53:27 +0800

    nginx

---
+++ lesson-distributed/chapter-nginx/README.md
@@ -0,0 +1,62 @@
+# Nginx
+
+[TOC]
+
+
+
+## Quickstart
+
+### 安装
+
+* `./configure --prefix=/usr/local/nginx`
+
+* 依赖
+
+  ```shell
+  yum install pcre-devel / zlib-devel
+  ```
+
+* `make && make install `
+
+* 添加模块 
+
+  ```shell
+  ./configure --prefix=/usr/local/nginx --with-http_stub_status_module  // prefix必须要和之前一样
+  make // 必须先执行make
+  cp objs/nginx nginx/sbin/nginx  // 替换原来的nginx
+  ```
+
+
+
+
+### 基本命令
+
+* 启动 `./nginx `
+* 关闭`./nginx -s stop`
+* 重启 `./nginx -s reload`
+
+
+
+## 课程目录
+
+1. **Nginx在分布式架构中所处的位置和作用**
+2. 正向代理和反向代理
+3. 常见的web服务器
+4. Nginx的安装部署
+5. Nginx配置文件分析
+6. location匹配规则
+7. 实际使用建议
+8. Rewrite规则
+
+
+
+## 衍生问题
+
+### TPS和QPS
+
+
+
+
+
+
+
--- lesson-structure/chapter-structure-linked/src/test/java/com/lesson/AppTest.java
+++ lesson-structure/chapter-structure-linked/src/test/java/com/lesson/AppTest.java
@@ -12,9 +12,32 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
+
+
+
+    private int a;
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        String[] office ={"律所1","律所2","律所3","律所4"};
+
+
+        String[][] classes = {{"律师1","律师2","律师3"},
+                {"律师1","律师2","律师3"},
+                {"律师1","律师2","律师3"},
+                {"律师1","律师2","律师3"},
+                {"律师1","律师2","律师3"}
+        };
+        forEach(office,classes);
+    }
+    private void forEach(String[]  classes,String[][] en){
+        for (int i=0;i<classes.length;i++) {
+            System.out.println(classes[i]+" " +en[i][a]);
+        }
+        a++;
+        if (en[0].length > a){
+            forEach(classes,en);
+        }
     }
 }
