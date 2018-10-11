55b26adda3dc518c01c969ec43b189a2ec8ccfdf
zhengshijun <zsjdxc251@sina.com>
2018-10-12 00:01:51 +0800

    代理模式补充

--- lesson-source/chapter-pattern/chapter-pattern-proxy/src/main/java/com/lesson/source/pattern/proxy/jdk/ProcessInvocationHandler.java
+++ lesson-source/chapter-pattern/chapter-pattern-proxy/src/main/java/com/lesson/source/pattern/proxy/jdk/ProcessInvocationHandler.java
@@ -1,15 +1,24 @@
 package com.lesson.source.pattern.proxy.jdk;
 
+import com.lesson.source.pattern.proxy.IUserInfoService;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class ProcessInvocationHandler implements InvocationHandler {
 
 
+    private IUserInfoService userInfoService;
+
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
         System.out.println(method.getName());
+
+
+        method.invoke(userInfoService,args);
+
         return "你好";
     }
 }
