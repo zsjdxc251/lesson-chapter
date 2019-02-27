8749d0cf9eacbbf925376ff0dee0b3a566f78878
zhengshijun <zsjdxc251@sina.com>
2019-02-27 23:16:31 +0800

    mybatis plus

---
+++ lesson-source/chapter-mybatis/chapter-mybatis-plus/src/main/java/com/lesson/source/mybatis/plus/MybatisPlusBootstrap.java
@@ -0,0 +1,18 @@
+package com.lesson.source.mybatis.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengshijun
+ * @version created on 2019/2/27.
+ */
+@SpringBootApplication
+public class MybatisPlusBootstrap {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(MybatisPlusBootstrap.class,args);
+
+    }
+}
