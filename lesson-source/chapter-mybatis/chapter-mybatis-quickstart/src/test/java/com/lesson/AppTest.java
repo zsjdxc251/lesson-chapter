ef7c38e4bde614215ba15e0f8446647c28acc10b
zhengshijun <zhengsj@akulaku.com>
2019-04-12 18:29:14 +0800

    base64

--- lesson-source/chapter-mybatis/chapter-mybatis-quickstart/src/test/java/com/lesson/AppTest.java
+++ lesson-source/chapter-mybatis/chapter-mybatis-quickstart/src/test/java/com/lesson/AppTest.java
@@ -10,38 +10,36 @@ import static org.junit.Assert.assertTrue;
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
 
 
-    public static void getName(){
+	public static void getName() {
 
-    }
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	}
 
-
-    @Test
-    public void testModifier() throws Exception{
-
-        System.out.println(ModifierService.class.getMethod("demo").getDeclaringClass().isInterface());
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
 
 
+	@Test
+	public void testModifier() throws Exception {
 
-        System.out.println(StringTools.toBinaryString(Modifier.ABSTRACT));
-        System.out.println(StringTools.toBinaryString(Modifier.PUBLIC));
-        System.out.println(StringTools.toBinaryString(Modifier.STATIC));
-        System.out.println(Modifier.ABSTRACT);
-        System.out.println(Modifier.PUBLIC);
-        System.out.println(Modifier.STATIC);
-        System.out.println(ModifierService.class.getMethod("demo").getModifiers() &(Modifier.ABSTRACT|Modifier.PUBLIC|Modifier.STATIC));
+		System.out.println(ModifierService.class.getMethod("demo").getDeclaringClass().isInterface());
 
 
-    }
+		System.out.println(StringTools.toBinaryString(Modifier.ABSTRACT));
+		System.out.println(StringTools.toBinaryString(Modifier.PUBLIC));
+		System.out.println(StringTools.toBinaryString(Modifier.STATIC));
+		System.out.println(Modifier.ABSTRACT);
+		System.out.println(Modifier.PUBLIC);
+		System.out.println(Modifier.STATIC);
+		System.out.println(ModifierService.class.getMethod("demo").getModifiers() & (Modifier.ABSTRACT | Modifier.PUBLIC | Modifier.STATIC));
+
+
+	}
 }
