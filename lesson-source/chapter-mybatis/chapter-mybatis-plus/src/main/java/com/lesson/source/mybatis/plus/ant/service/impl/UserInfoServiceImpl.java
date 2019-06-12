6b6aafa75527b6f94c6e0d03e1703972bc5a32ec
Stream <zsjdxc251@live.com>
2019-06-12 22:48:39 +0800

    mybatis plus

--- lesson-source/chapter-mybatis/chapter-mybatis-plus/src/main/java/com/lesson/source/mybatis/plus/ant/service/impl/UserInfoServiceImpl.java
+++ lesson-source/chapter-mybatis/chapter-mybatis-plus/src/main/java/com/lesson/source/mybatis/plus/ant/service/impl/UserInfoServiceImpl.java
@@ -1,10 +1,16 @@
 package com.lesson.source.mybatis.plus.ant.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.additional.query.impl.LambdaQueryChainWrapper;
 import com.lesson.source.mybatis.plus.ant.entity.UserInfo;
 import com.lesson.source.mybatis.plus.ant.mapper.UserInfoMapper;
 import com.lesson.source.mybatis.plus.ant.service.IUserInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements IUserInfoService {
 
+
+
+    public void laSelect(){
+
+        LambdaQueryWrapper<UserInfo> lambda1 = new QueryWrapper<UserInfo>().lambda();
+        LambdaQueryWrapper<UserInfo> lambda2 = new LambdaQueryWrapper<>();
+
+        LambdaQueryWrapper<UserInfo> lambda3 =  Wrappers.lambdaQuery();
+
+        lambda1.ge(UserInfo::getAge,1);
+
+        lambda1.select()
+        List<UserInfo> list = new LambdaQueryChainWrapper<>(baseMapper).gt(UserInfo::getPassword,"1231231").list();
+
+    }
 }
