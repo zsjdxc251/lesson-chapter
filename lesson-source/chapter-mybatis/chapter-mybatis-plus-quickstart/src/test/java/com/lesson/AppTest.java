4dd95b90e59698d25b15e46fc0d11f38a180b48a
zhengshijun <zhengsj@akulaku.com>
2019-08-19 10:13:52 +0800

    Lambda

--- lesson-source/chapter-mybatis/chapter-mybatis-plus-quickstart/src/test/java/com/lesson/AppTest.java
+++ lesson-source/chapter-mybatis/chapter-mybatis-plus-quickstart/src/test/java/com/lesson/AppTest.java
@@ -2,6 +2,7 @@ package com.lesson;
 
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.toolkit.ExceptionUtils;
+import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
 import com.baomidou.mybatisplus.core.toolkit.SerializationUtils;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.baomidou.mybatisplus.core.toolkit.support.SerializedLambda;
@@ -24,7 +25,11 @@ public class AppTest {
 	public void shouldAnswerWithTrue() {
 
 
-		test1(User::getPhoneNumber);
+
+
+		SerializedLambda serializedLambda = LambdaUtils.resolve(User::getPhoneNumber);
+
+		System.out.println(serializedLambda.getImplMethodName());
 
 
 	}
@@ -38,7 +43,10 @@ public class AppTest {
 			}
 		}) {
 
-			System.out.println(JSON.toJSONString(objIn.readObject(), true));
+			SerializedLambda serializedLambda = (SerializedLambda)objIn.readObject();
+
+
+			System.out.println(serializedLambda.getImplMethodName());
 		} catch (Exception e) {
 			throw ExceptionUtils.mpe("This is impossible to happen", e);
 		}
